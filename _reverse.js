107f4733243c0c7996554263b33fb1d2bf73a453
conorjohn <conorj92@gmail.com>
2019-04-13 23:16:21 +0100

    feature: _reverse function is working albeit a little rough

--- _reverse.js
+++ _reverse.js
@@ -1,5 +1,12 @@
 const chunk = (array) => {
     // Reverses array so that the first element becomes the last, 
     // the second element becomes the second to last, and so on.
-    
+    let newArray = [];
+    for (let i = array.length - 1; i > 0; i--) {
+        newArray.push(array[i]);
+    }
+    console.log('After loop: ', newArray);
+    // Maybe we should use .splice here
 }
+
+chunk([0,1,2,3,4,5,6,7,8]);
