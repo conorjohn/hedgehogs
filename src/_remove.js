5455147cd8cb93340bda1566950e6fcb6578bbf0
conorjohn <conorj92@gmail.com>
2019-05-22 19:57:05 +0100

    chore: groundwork laid

---
+++ src/_remove.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param {Array} array
+ * @return {*}
+ * 
+ * @example
+ *  remove([1, 2, 3, 4], (n) => {
+ *   return n % 2 === 0;
+ *  })
+ */
+const remove = (array, fn) => {
+    
+}
