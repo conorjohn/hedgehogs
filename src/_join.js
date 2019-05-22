2ce7054acfef76f31752e3d89d3c868166a1c051
conorjohn <conorj92@gmail.com>
2019-05-22 20:17:37 +0100

    feature: concatenating an array into a string with a separator

---
+++ src/_join.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @param {Array} array 
+ * @param {String} separator 
+ * 
+ * This function returns a string
+ * @example
+ *  join(['one', 'two', 'three'], '~');
+ *  'one~two~three'
+ */
+const join = (array, separator = ' ') => {
+    let str = '';
+    array.map((item, i) => {
+        i < array.length - 1 ? str += item + '' + separator : str += item;
+    });
+    
+    console.log(str);
+}
+
+join(['string', 'number', 'boolean'], '~');
