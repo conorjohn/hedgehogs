34d17155620c9d7e4e2a96446c11de6b103309b5
conorjohn <conorj92@gmail.com>
2019-05-22 20:21:57 +0100

    chore: tidy up and configuration

--- src/_join.js
+++ src/_join.js
@@ -8,13 +8,11 @@
  *  join(['one', 'two', 'three'], '~');
  *  'one~two~three'
  */
+
 const join = (array, separator = ' ') => {
     let str = '';
     array.map((item, i) => {
         i < array.length - 1 ? str += item + '' + separator : str += item;
     });
-    
-    console.log(str);
+    return str;
 }
-
-join(['string', 'number', 'boolean'], '~');
